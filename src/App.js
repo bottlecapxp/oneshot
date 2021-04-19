9ca3b6ea753efc19194c13cb572d2b31f6a9905f
Decory <decory@breezemaxweb.com>
2021-04-19 15:08:49 -0400

    Last test on app env

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          OneShot Changes
+          OneShot Change
         </a>
       </header>
     </div>
